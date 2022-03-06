8791f31ad261d03c47a5239c9cdb43d443d620ab
swapnilsparsh <arezona.lucky55@gmail.com>
2022-03-07 02:29:48 +0530

    Sign Up Auth Done

--- lib/AllScreens/registrationScreen.dart
+++ lib/AllScreens/registrationScreen.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hexclan_user/AllScreens/loginScreen.dart';
+import 'package:hexclan_user/AllScreens/mainscreen.dart';
+import 'package:hexclan_user/main.dart';
 
 class RegistrationScreen extends StatelessWidget {
+
   static const String idScreen = "register";
+
   TextEditingController nameTextEditingController = TextEditingController();
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController phoneTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -118,7 +125,21 @@ class RegistrationScreen extends StatelessWidget {
                         borderRadius: new BorderRadius.circular(24.0),
                       ),
                       onPressed: () {
-                        // registerNewUser(context);
+                        if(nameTextEditingController.text.length < 3){
+                          displayToastMessage("name must be atleast 3 characters", context);
+                        }
+                        // else if(emailTextEditingController.text.contains("@")){
+                        //   displayToastMessage("Email Address is not valid", context);
+                        // }
+                        else if(phoneTextEditingController.text.isEmpty){
+                          displayToastMessage("Phone number is mandatory", context);
+                        }
+                        else if(passwordTextEditingController.text.length < 6){
+                          displayToastMessage("Password must be atleast 6 characters", context);
+                        }
+                        else{
+                          registerNewUser(context);
+                        }
                       },
                     )
                   ],
@@ -140,24 +161,38 @@ class RegistrationScreen extends StatelessWidget {
     );
   }
 
-  // final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  // registerNewUser(BuildContext context) async {
-  //   final FirebaseUser firebaseUser = (await _firebaseAuth
-  //   .createUserWithEmailAndPassword(
-  //     email: emailTextEditingController.text,
-  //     password: passwordTextEditingController.text)).user;
-  // }
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  
+  registerNewUser(BuildContext context) async
+  {
+    final User firebaseUser = (await _firebaseAuth
+    .createUserWithEmailAndPassword(
+      email: emailTextEditingController.text,
+      password: passwordTextEditingController.text
+      ).catchError((errMsg){
+        displayToastMessage("Error: " + errMsg.toString(), context);
+      })).user;
 
+      if(firebaseUser != null)
+      {
+        Map userDataMap = {
+          "name": nameTextEditingController.text.trim(),
+          "email": emailTextEditingController.text.trim(),
+          "phone": phoneTextEditingController.text.trim(),
+        };
 
-  // if(firebaseUser != null) //user created
-  // {
-  //   //save user info to database
-  // }
-  // else //user not created
-  // {
-  //   //show error message
-  // }
+        usersRef.child(firebaseUser.uid).set(userDataMap);
+        displayToastMessage("Congratulations, your account has been created", context);
 
+        Navigator.pushNamedAndRemoveUntil(
+            context, MainScreen.idScreen, (route) => false);
+      }
+      else{
+        displayToastMessage("New User account has not been created", context);
+      }
+  }
+  displayToastMessage(String message, BuildContext context)
+  {
+    Fluttertoast.showToast(msg: message);
+  }
 }
